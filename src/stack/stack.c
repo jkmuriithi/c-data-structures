c3ce8010cee8ad3d4ccf6bd4435a0b9de43189dc
Jude Muriithi <muriithi@princeton.edu>
2023-05-29 23:39:37 -0700

    Complete initial stack implementation

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -9,41 +9,93 @@
 #include "stack.h"
 
 #define C_GEN_STACK_DEFAULT_SIZE 10
+#define C_GEN_STACK_GROWTH_FACTOR 1.5
+#define C_GEN_STACK_MIN_SIZE 1
 
 struct Stack {
-    /**
-     * Number of items currently on the stack. Doubles as a pointer to the
-     * top of the stack.
-     */
+    /** Number of items currently held by the stack. */
     size_t count;
     /** Holds stack items. */
     void** items;
-    /** Length of **items. */
     size_t items_len;
 };
 
 Stack* Stack_create(void) {
     Stack* s = malloc(sizeof(Stack));
-    if (s == NULL) return s;
+    if (s == NULL) return NULL;
 
     s->count = 0;
+    s->items_len = C_GEN_STACK_DEFAULT_SIZE;
     s->items = malloc(sizeof(void*) * C_GEN_STACK_DEFAULT_SIZE);
     if (s->items == NULL) {
         free(s);
         return NULL;
     }
-    s->items_len = C_GEN_STACK_DEFAULT_SIZE;
 
     return s;
 }
 
 size_t Stack_count(Stack* s) {
     if (s == NULL) return 0;
-    else return s->count;
+    return s->count;
 }
 
 bool Stack_empty(Stack* s) {
-    return Stack_count(s) == 0;
+    if (s == NULL) return true;
+    return s->count == 0;
 }
 
+/**
+ * @brief Resizes the item array of the stack pointed to by s. Returns false iff
+ * the operation could not be completed successfully. Should not be called with
+ * NULL s.
+ */
+static bool resize(Stack* s, size_t new_length) {
+    void** new_alloc = realloc(s->items, new_length);
+    if (new_alloc == NULL) return false;
 
+    s->items = new_alloc;
+    s->items_len = new_length;
+    return true;
+}
+
+bool Stack_push(Stack* s, void* entry) {
+    if (s == NULL) return false;
+
+    // Grow items array if necessary
+    if (s->count == s->items_len) {
+        size_t new_length = (size_t) (s->items_len * C_GEN_STACK_GROWTH_FACTOR);
+        if (new_length < s->items_len) return false; // Overflow check
+
+        if (!resize(s, new_length)) return false;
+    }
+
+    s->items[s->count++] = entry;
+    return true;
+}
+
+void* Stack_pop(Stack* s) {
+    if (Stack_empty(s)) return NULL;
+    return s->items[--s->count];
+}
+
+void* Stack_peek(Stack* s) {
+    if (Stack_empty(s)) return NULL;
+    return s->items[s->count - 1];
+}
+
+bool Stack_trim(Stack* s) {
+    if (s == NULL) return false;
+
+    // Trim down items array to max(MIN_SIZE, count)
+    size_t new_length;
+    if (s->count < C_GEN_STACK_MIN_SIZE) new_length = C_GEN_STACK_MIN_SIZE;
+    else new_length = s->count;
+
+    return resize(s, new_length);
+}
+
+void Stack_destroy(Stack* s) {
+    free(s->items);
+    free(s);
+}
--- src/stack/stack.h
+++ src/stack/stack.h
@@ -13,42 +13,57 @@
 struct Stack;
 
 /**
- * Generic stack data structure, designed to hold data in void pointer form.
- * A pointer to a properly allocated stack will never be NULL.
+ * @brief Generic stack data structure, designed to hold data in void pointer
+ * form. A pointer to a properly allocated stack will never be NULL.
  */
 typedef struct Stack Stack;
 
 /**
- * Creates a new stack pointer. If memory cannot be allocated, the returned
+ * @brief Creates a new stack pointer. If memory cannot be allocated, the returned
  * pointer will be NULL. Must be destroyed before program termination using
  * Stack_destroy.
  */
 Stack* Stack_create(void);
 
 /**
- * Returns the number of items currently held in stack *s. Returns 0 if s is
+ * @brief Returns the number of items currently held in stack *s. Returns 0 if s is
  * NULL.
  */
 size_t Stack_count(Stack* s);
 
-/** Returns true if s points to an empty stack or is NULL. */
+/** @brief Returns true if s points to an empty stack or is NULL. */
 bool Stack_empty(Stack* s);
 
 /**
- * Pushes an item onto stack *s. Returns true if the operation was successful,
- * and false if the operation was unsuccessful. Returns false if s is NULL.
+ * @brief Pushes an item onto stack *s. Returns true if the operation was
+ * successful, and false if either the operation was unsuccessful or s is NULL.
  */
 bool Stack_push(Stack* s, void* entry);
 
 /**
- * Retrieves the item at the top of stack *s. Returns NULL if the stack is
- * empty, or if s is NULL.
+ * @brief Retrieves the item at the top of stack *s and removes it. Returns NULL
+ * if the stack is empty, the operation was unsuccessful, or if s is NULL.
  */
 void* Stack_pop(Stack* s);
 
 /**
- * Destroys the stack pointed to by s. If s is NULL, no operation is
- * performed.
+ * @brief Retrieves the item at the top of stack *s, but does not remove it.
+ * Returns NULL if the stack is empty, or if s is NULL.
+ */
+void* Stack_peek(Stack* s);
+
+/**
+ * @brief Trims the size of a stack in memory down to its current length.
+ * Returns true if the operation was successful. Returns false if the
+ * operation was unsuccessful or if s is NULL. If an implementation enforces
+ * some minimum memory allocation size, this function will return a stack of
+ * at least that size in memory.
+ */
+bool Stack_trim(Stack* s);
+
+/**
+ * @brief Destroys the stack pointed to by s, freeing all memory allocated to
+ * the structure. If s is NULL, no operation is performed.
  */
 void Stack_destroy(Stack* s);
 
